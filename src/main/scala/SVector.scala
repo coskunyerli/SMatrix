278c2fef5be1487d128b1589a29528c98be1860b
Coskun Yerli <gomez@cavevfx.com>
2018-07-15 20:24:34 +0300

    Implenmented methods and added new methods in this class

--- src/main/scala/SVector.scala
+++ src/main/scala/SVector.scala
@@ -1,6 +1,6 @@
 import scala.util.Random
 
-case class SVector(private val array: Array[Double]) {
+class SVector(private val array: Array[Double]) {
 
   def apply(i: Int): Double = this.array(i)
 
@@ -36,13 +36,31 @@ case class SVector(private val array: Array[Double]) {
 
   def *(s: Double): SVector = SVector(this.array.map(_ * s))
 
-  def *(mat: SMatrix): SVector = mat * this
+  def *:(vec: SVector) = SVector(this.array.zip(vec.array).map(tuple => tuple._1 * tuple._2))
 
-  def /(s: Double): SVector = this * (1.0 / s)
+  def ^(s: Double): SVector = SVector(this.array.map(n => Math.pow(n, s)))
+
+  def *(mat: SMatrix): SVector = {
+    if (mat.cols != this.length) {
+      throw new Exception("Matrix size is not equal with vector")
+    }
+    var zeros = SVector.zeros(this.length)
+    mat.vecs.zip(this.array).foreach { tuple =>
+      zeros = zeros + tuple._1 * tuple._2
+    }
+    zeros
+  }
+
+  def /(s: Double): SVector = {
+    if (s == 0.0) {
+      throw new Exception("Vector is not divided by zero")
+    }
+    this * (1.0 / s)
+  }
 
   def norm: Double = Math.sqrt(this.dot(this))
 
-  def length: Double = norm
+  def length: Int = dim
 
   def unit: SVector = this / this.length
 
@@ -52,23 +70,48 @@ case class SVector(private val array: Array[Double]) {
 
   def foreach(f: (Double) => Unit): Unit = this.array.foreach(f)
 
-  def foldLeft = ???
+  def foldLeft[B](s: B)(f: (B, Double) => B): B = this.array.foldLeft(s)(f)
+
+  def sum: Double = this.array.foldLeft(0.0)(_ + _)
 
   def isPerpendicular(vec: SVector): Boolean = (this dot vec) <= 0.0
 
+  def toMatrix(h: Boolean = false): SMatrix = {
+    if (h) {
+      SMatrix(this.array.map(num => SVector(Array(num))))
+    } else {
+      SMatrix(Array(this))
+    }
+  }
+
   override def toString: String = new StringBuilder("[ ").append(this.array.mkString(", ")).append(" ]").toString()
+
+  def data: Array[Double] = this.array
 
 }
 
 object SVector {
 
-  // TODO negative or zero dimention exception
-  def zeros(size: Int): SVector = SVector(Array.fill[Double](size)(0.0))
+  def zeros(size: Int): SVector = {
+    if (size <= 0) {
+      throw new DimensionException("Vector can not be created because of size")
+    }
+    SVector(Array.fill[Double](size)(0.0))
+  }
 
-  // TODO negative or zero dimention exception
-  def ones(size: Int): SVector = SVector(Array.fill[Double](size)(1.0))
+  def ones(size: Int): SVector = {
+    if (size <= 0) {
+      throw new DimensionException("Vector can not be created because of size")
+    }
+    SVector(Array.fill[Double](size)(1.0))
+  }
 
-  def random(size: Int): SVector = SVector(Array.fill[Double](size)(Random.nextDouble))
+  def random(size: Int): SVector = {
+    if (size <= 0) {
+      throw new DimensionException("Vector can not be created because of size")
+    }
+    SVector(Array.fill[Double](size)(Random.nextDouble))
+  }
 
   def unitRandom(size: Int): SVector = SVector.random(size).normalize
 
